0314f3578e9446632da288db24ca2ac908642a09
Hannah Brog <bugsbrog@gmail.com>
2023-05-04 16:40:22 -0600

    Done with part 1

--- arrays.php
+++ arrays.php
@@ -6,6 +6,15 @@
     <title>untitled</title>
   </head>
   <body>
-
+    Part 1.
+    <br />
+    <?php 
+        $string = array("rain", "sunshine", "clouds", "hail",
+        "sleet", "snow", "wind");
+        echo "We've seen all kinds of weather this month. At the 
+        beginning of the month, we had $string[5] and $string[6]. 
+        Then came $string[1] with a few $string[2] and some $string[0]. 
+        At least we didn't get any $string[3] or $string[4].";
+    ?>
   </body>
 </html>
