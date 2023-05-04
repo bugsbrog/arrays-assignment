14dbff28d5b85b26b1d5d1b46d19c3aff42e1343
Hannah Brog <bugsbrog@gmail.com>
2023-05-04 17:06:31 -0600

    Done with part 2

--- arrays.php
+++ arrays.php
@@ -1,20 +1,37 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-   "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
 <html lang="en">
-  <head>
+
+<head>
     <title>Arrays Assignment</title>
-  </head>
-  <body>
+</head>
+
+<body>
     Part 1.
     <br />
-    <?php 
-        $string = array("rain", "sunshine", "clouds", "hail",
-        "sleet", "snow", "wind");
-        echo "We've seen all kinds of weather this month. At the 
+    <?php
+    $string = array(
+        "rain", "sunshine", "clouds", "hail",
+        "sleet", "snow", "wind"
+    );
+    echo "We've seen all kinds of weather this month. At the 
         beginning of the month, we had $string[5] and $string[6]. 
         Then came $string[1] with a few $string[2] and some $string[0]. 
         At least we didn't get any $string[3] or $string[4].";
-    ?>
-  </body>
+    ?><br />
+    Part 2.
+    <br />
+
+    <?php $assoc = array(
+        "Japan" => "Tokyo", "Mexico" => "Mexico City",
+        "USA" => "New York City", "India" => "Mumbai", "South Korea" => "Seoul",
+        "China" => "Shanghai", "Nigeria" => "Lagos", "Argentina" => "Buenos 
+Aires", "Egypt" => "Cairo", "England" => "London"
+    ); ?>
+    <?php foreach ($assoc as $country => $city) {
+        echo $city . ", " . $country . "<br />";
+    } ?><br />
+
+</body>
+
 </html>
